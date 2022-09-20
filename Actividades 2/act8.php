25763d20a025b2df45e8f876cfe35c4eb256d09f
lorca03 <guillermolorcamartinez@gmail.com>
2022-09-20 08:49:15 +0200

    actividades2

--- Actividades 2/act8.php
+++ Actividades 2/act8.php
@@ -1,8 +1,13 @@
 <?php
+    date_default_timezone_set("Europa");
     $nac=$_POST["Nacimiento"];
     $nac=strtotime($nac);
     $hoy= date("U");
-    $edad=$hoy-$nac-1970;
-    $edad= gmdate("Y",$edad);
-    echo $edad-1970;
+    $edad=$hoy-$nac;
+    $años= intval($edad/31536000);
+    $meses =intval(($edad%31536000)/2592000);
+    $dias =intval((($edad%31536000)%2592000)/86400);
+    $horas =intval(((($edad%31536000)%2592000)%86400)/3600);
+    $seg =intval(((($edad%31536000)%2592000)%86400)%3600);
+    echo "La edad son  ".$años." años,".$meses." meses,".$dias." dias,".$horas." horas,".$seg." segundos";
     
