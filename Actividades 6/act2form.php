b27139272ed0046cf5c3409e616ed7ead6fb8cd6
lorca03 <guillermolorcamartinez@gmail.com>
2022-09-27 22:42:26 +0200

    act2

--- Actividades 6/act2form.php
+++ Actividades 6/act2form.php
@@ -11,7 +11,7 @@
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="estilo.css">
-</head>
+</head> 
 
 <body>
     <?php
@@ -20,7 +20,7 @@
     }
 
     ?>
-
+ 
     <div class="container">
         <div class="abs-center">
             <form method="post" enctype="multipart/form-data" action="act2subir.php" class="border p-3 form">
--- Actividades 6/act2subir.php
+++ Actividades 6/act2subir.php
@@ -6,7 +6,13 @@ $nombre=$archivo["name"];
 
 if ($tipo == "image/png" ||$tipo == "image/jpg" ||$tipo == "image/jpeg" ||$tipo == "image/gif") {
     if (!is_dir("imagenes")) {
-        mkdir("imagenes",0777);
+        mkdir("imagenes", 0777);
     }
-    move_uploaded_file($nombre,"imagenes/$nombre");
+    $enviada=move_uploaded_file($nombre,"/imagenes/$nombre");
+    if ($enviada) {
+        echo "Se ha guardado con exito";
+    } else {
+        echo "No se ha podido guardar";
+    }
+   // header("refresh:5,url=http://localhost/Servidor/Actividades%206/act2form.php");
 }
